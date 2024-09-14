a5d87b5582295ef13283343879de3a3a6d7f6e3c
JoahPeterson <118687169+JoahPeterson@users.noreply.github.com>
2024-09-14 17:16:00 -0500

    Edited Course model to include validation attributes and changed TermId to Term. Also remoevd DateCreated property from the model since its now in the auditable class. Tasks #31 #32

--- TimetableApp/TimetableApp.DataModels/Models/Course.cs
+++ TimetableApp/TimetableApp.DataModels/Models/Course.cs
@@ -1,4 +1,6 @@
-﻿namespace TimetableApp.DataModels.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimetableApp.DataModels.Models;
 
 public class Course
 {
@@ -6,17 +8,21 @@ public class Course
 
     public Auditable AuditInformation { get; set; }
 
-    public DateTime? DateCreated { get; set; }
-
+    [Required(ErrorMessage = "Description is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Description must be between 3 and 100 characters.")]
     public string? Description { get; set; }
 
+    [DataType(DataType.Date, ErrorMessage ="Value must be a valid date.")]
     public DateTime EndDate { get; set; }
 
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 30 characters.")]
     public string Name { get; set; }
 
+    [DataType(DataType.Date, ErrorMessage = "Value must be a valid date.")]
     public DateTime StartDate { get; set; }
 
-    public string TermId { get; set; }
+    public Term Term { get; set; }
 
     public List<WorkUnit> WorkUnits { get; set; }
 
