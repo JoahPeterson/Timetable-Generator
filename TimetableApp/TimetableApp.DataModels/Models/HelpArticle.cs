447f3185fe23d7cf10edb8b46d47ac95c3e785c4
Chris Cremeens <12780624@mstc.edu>
2024-09-27 17:37:21 -0500

    #66 Added data access layer for the HelpArticle.

---
+++ TimetableApp/TimetableApp.DataModels/Models/HelpArticle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableApp.DataModels.Models;
+public class HelpArticle
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public Auditable AuditInformation { get; set; } = new Auditable();
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Tittle must be between 3 and 100 characters.")]
+    public string? Title { get; set; }
+
+    [Required(ErrorMessage = "Content is required")]
+    public string? Content { get; set; }
+
+}
