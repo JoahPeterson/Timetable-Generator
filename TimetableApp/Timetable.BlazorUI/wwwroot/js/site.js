314b0903ef4f13b519db60f2bc2e61f1635e3691
JoahPeterson <118687169+JoahPeterson@users.noreply.github.com>
2024-09-29 20:14:47 -0500

    Task #60 made tasks draggable into work units where they are assigned as workunit tasks.

--- TimetableApp/Timetable.BlazorUI/wwwroot/js/site.js
+++ TimetableApp/Timetable.BlazorUI/wwwroot/js/site.js
@@ -5,3 +5,13 @@
 window.preventDefault = function (event) {
     event.preventDefault();
 };
+
+
+function SetSession(name, taskId) {
+    localStorage.setItem(name, taskId);
+}
+
+function GetSession(name)
+{
+    return localStorage.getItem(name);
+}
