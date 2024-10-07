fed4924d5a2c4a64160cbb55c98363808f516568
JoahPeterson <118687169+JoahPeterson@users.noreply.github.com>
2024-10-07 17:31:19 -0500

    Task #78 [Peterson + Cremeens] Updated blazor modals to use micromodal js library to fix form save issues Cleaned up commented out code and unused code from project.

--- TimetableApp/Timetable.BlazorUI/wwwroot/js/site.js
+++ TimetableApp/Timetable.BlazorUI/wwwroot/js/site.js
@@ -8,16 +8,14 @@ window.preventDefault = function (event) {
     event.preventDefault();
 };
 
-// modalInterop.js
-
-//document.addEventListener('DOMContentLoaded', function () {
-//    MicroModal.init(); // Initialize MicroModal
-//});
 
 window.initializeModal = () => {
     MicroModal.init();
 
     const closeModalButton = document.getElementById('closeModalButton');
+
+    const btnName = document.getElementById('closeModalButton').getAttribute('name');
+    
 
     window.modalInterop = {
         // Show the modal by its ID
@@ -33,25 +31,12 @@ window.initializeModal = () => {
 
     if (closeModalButton) {
         closeModalButton.addEventListener('click', function () {
-            window.modalInterop.hideModal('modal-1');
+            window.modalInterop.hideModal(btnName);
         });
     } else {
         console.warn('Modal button not found! Make sure the Blazor component has rendered.');
     }
 };
-
-//window.modalInterop = {
-//    // Show the modal by its ID
-//    showModal: function (modalId) {
-//        MicroModal.show(modalId); // Show the modal
-//    },
-
-//    // Hide the modal by its ID
-//    hideModal: function (modalId) {
-//        MicroModal.close(modalId); // Hide the modal
-//    }
-//};
-
 
 function SetSession(name, taskId) {
     localStorage.setItem(name, taskId);
