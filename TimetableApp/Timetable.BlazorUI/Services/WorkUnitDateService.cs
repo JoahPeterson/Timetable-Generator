71856dfe8da7c4fc02f74ff66feddbe263b4345a
Chris Cremeens <12780624@mstc.edu>
2024-10-26 13:53:31 -0500

    #146 Added due date calculation logic to the WorkUnit form submition and the CourseCreateEdit submittion.

--- TimetableApp/Timetable.BlazorUI/Services/WorkUnitDateService.cs
+++ TimetableApp/Timetable.BlazorUI/Services/WorkUnitDateService.cs
@@ -1,4 +1,6 @@
-﻿namespace Timetable.BlazorUI;
+﻿using TimetableApp.DataModels.Models;
+
+namespace Timetable.BlazorUI;
 
 public class WorkUnitDateService
 {
@@ -49,11 +51,57 @@ public class WorkUnitDateService
             if (startDate == workUnit.StartDate && endDate == workUnit.EndDate)
                 continue;
 
+            int differenceInStartDays = (startDate.Date - workUnit.StartDate)?.Days ?? 0;
+
+            workUnit.StartDate = startDate.Date;
+            workUnit.EndDate = endDate.Date;
+
+            if (differenceInStartDays != 0)
+            {
+                var updatedWorkUnit = await UpdateWorkTask(workUnit, differenceInStartDays);
+                await _workUnitData.UpdateAsync(workUnit);
+            }
+            
+            await _workUnitData.UpdateAsync(workUnit);
+        }
+        return course;
+    }
+
+    public async Task<Course> UpdateWorkUnitStartAndEndDateAndWorkTask(Course oldCourse, Course newCourse)
+    {
+        int differenceInStartDays = (newCourse.StartDate.Date - oldCourse.StartDate.Date).Days;
+        
+        int weekCount = 0;
+
+        foreach (WorkUnit workUnit in newCourse.WorkUnits)
+        {
+            DateTime startDate = newCourse.StartDate.Date.AddDays(weekCount * 7).Date;
+            DateTime endDate = startDate.AddDays(workUnit.Duration * 7).Date;
+
+            weekCount += workUnit.Duration;
+
+            if (startDate == workUnit.StartDate && endDate == workUnit.EndDate)
+                continue;
+
             workUnit.StartDate = startDate.Date;
             workUnit.EndDate = endDate.Date;
 
             await _workUnitData.UpdateAsync(workUnit);
         }
-        return course;
+
+        foreach (WorkUnit workUnit in newCourse.WorkUnits)
+        {
+            await _workUnitData.UpdateAsync( await UpdateWorkTask(workUnit, differenceInStartDays));
+        }
+        return newCourse;
+    }
+
+    private async Task<WorkUnit> UpdateWorkTask(WorkUnit workUnit, int daySpread)
+    {
+        foreach (WorkUnitTask task in workUnit.Tasks)
+        {
+            task.DueDate = task.DueDate?.AddDays(daySpread);
+        }
+        return workUnit;
     }
 }
