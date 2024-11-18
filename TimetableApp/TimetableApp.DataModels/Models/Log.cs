c321ba84e172d069fe66e076dc26c4b4c60737fc
JoahPeterson <118687169+JoahPeterson@users.noreply.github.com>
2024-11-18 17:01:22 -0600

    Task #197 Added Data Model For Logs

---
+++ TimetableApp/TimetableApp.DataModels/Models/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableApp.DataModels.Models
+{
+    public class Log
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string? CallStack { get; set; }
+
+        public string? LoggedInUserId {  get; set; }
+
+        public string LogLevel { get; set; }
+
+        public string? Message { get; set; }
+
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+
+    }
+}
